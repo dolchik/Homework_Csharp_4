d7de84a1bb4358bf8df3a000d48364c7c01e562e
OLGA DRACH <dolchik88@mail.ru>
2022-12-19 16:47:07 +0300

    Программа возводит число А в степень В

---
+++ Ex_25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int EntryDigit(string str) // пользователь вводит число цифр в массиве и мы его считываем
+{
+Console.Write(str);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+int ExponenNum(int numbA, int numbB) //цикл возведения числа А в число В
+{
+    int result = 1;
+    for (int i = 0; i < numbB; i++)
+    {
+        result = result * numbA;
+    }
+    return result;
+}
+
+int numbA = EntryDigit("Введите число А: ");
+int numbB = EntryDigit("Введите числов В: ");
+int result = ExponenNum(numbA, numbB);
+Console.WriteLine($"{numbA}, {numbB} -> {result}");
