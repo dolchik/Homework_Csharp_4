bbacdf5d367fad92d101ad5cc084c3b13b3b83a6
OLGA DRACH <dolchik88@mail.ru>
2022-12-19 17:35:37 +0300

    Программа выдает сумму цифр в числе

---
+++ Ex_27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int EntryDigit(string str) // пользователь вводит число цифр в массиве и мы его считываем
+{
+Console.Write(str);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+int FindSum(int UserDigit) //сумма цифр в числе
+{
+    int sum;
+    for (sum = 0; UserDigit > 0; UserDigit = UserDigit / 10)
+    {
+        int count = UserDigit % 10; // остаток от деления
+        sum = sum + count;
+    }
+    return sum;
+}
+
+int number = EntryDigit("Введите число: ");
+int result = FindSum(number);
+Console.WriteLine($"{number} -> {result}");
